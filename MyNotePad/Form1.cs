a2d05450eeded1d860e12b54e422c993a14b6768
Ilshat Ibragimov <ibragimoff.ilshat2006@gmail.com>
2024-09-14 01:42:02 +0500

    Revert "Зеленая форма и удаление кнопки "Закрыть""
    This reverts commit 674c812421d1f6dcb6531d3c0ee9713444c8a4b3.

--- MyNotePad/Form1.cs
+++ MyNotePad/Form1.cs
@@ -17,5 +17,9 @@ namespace MyNotePad
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
